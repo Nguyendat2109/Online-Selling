37c18a10c3365eaec78c0d4e54fee83c6393a1fb
Nguyendat2109 <dat21092003@gmail.com>
2023-09-14 17:24:23 +0700

    first commit

---
+++ app/src/main/java/vn/edu/poly/banhangonline_ph26291/EventBus/Event_TongTienAll_Moi.java
@@ -0,0 +1,4 @@
+package vn.edu.poly.banhangonline_ph26291.EventBus;
+
+public class Event_TongTienAll_Moi {
+}
---
+++ app/src/main/java/vn/edu/poly/banhangonline_ph26291/Interface/ImageClick.java
@@ -0,0 +1,7 @@
+package vn.edu.poly.banhangonline_ph26291.Interface;
+
+import android.view.View;
+
+public interface ImageClick {
+    void onImageClick(View view,int pos,int giatri);
+}
